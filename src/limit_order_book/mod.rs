a375c1e10d3294f5065599ae15b75b53bff42e80
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-04-21 12:26:52 -0600

    OrderBook Update Id

--- src/limit_order_book/mod.rs
+++ src/limit_order_book/mod.rs
@@ -2,6 +2,17 @@ use super::{Asks, Bids};
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct LimitOrderBook {
+    update_id: usize,
     bids: Bids,
     asks: Asks,
+}
+
+impl LimitOrderBook {
+    pub fn new() -> LimitOrderBook {
+        LimitOrderBook {
+            update_id: 0,
+            bids: Bids::new(),
+            asks: Asks::new(),
+        }
+    }
 }
