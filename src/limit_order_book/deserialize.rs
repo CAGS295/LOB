0c9364efa4e248a03d2c58eeb2dbdb41329d393f
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-04-25 13:10:00 -0600

    DepthUpdate deserialization fitted to binance

--- src/limit_order_book/deserialize.rs
+++ src/limit_order_book/deserialize.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod test {
+    use crate::DepthUpdate;
     use crate::LimitOrderBook;
     use crate::PriceAndQuantity;
 
@@ -34,6 +35,56 @@ mod test {
         let book: LimitOrderBook = serde_json::from_str(snapshot).unwrap();
         let expected = LimitOrderBook {
             update_id: 17866404615,
+            bids: vec![
+                PriceAndQuantity(27826.1, 0.69556),
+                PriceAndQuantity(27826.89, 2.50099),
+            ]
+            .into(),
+            asks: vec![
+                PriceAndQuantity(27826.91000000, 0.26959000),
+                PriceAndQuantity(27826.90000000, 4.80586000),
+            ]
+            .into(),
+        };
+        assert_eq!(book, expected);
+    }
+
+    #[test]
+    fn deserialize_depth_update() {
+        let update = r#"
+            {
+                "e": "depthUpdate",
+                "E": 123456789,
+                "s": "BNBBTC",
+                "U": 157,
+                "u": 160,
+                "b": [
+                    [
+                        "27826.89000000",
+                        "2.50099000"
+                    ],
+                    [
+                        "27826.10000000",
+                        "0.69556000"
+                    ]
+                ],
+                "a": [
+                   [
+                        "27826.90000000",
+                        "4.80586000"
+                    ],
+                    [
+                        "27826.91000000",
+                        "0.26959000"
+                    ]
+                ]
+            }
+        "#;
+        let book: DepthUpdate = serde_json::from_str(update).unwrap();
+        let expected = DepthUpdate {
+            timestamp: 123456789,
+            first_update_id: 157,
+            last_update_id: 160,
             bids: vec![
                 PriceAndQuantity(27826.1, 0.69556),
                 PriceAndQuantity(27826.89, 2.50099),
