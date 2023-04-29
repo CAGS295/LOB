12921e4dddbdddd2bcd28bd7e213931b958a6c2d
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-04-28 19:21:39 -0600

    expose bids and asks add methods

--- src/limit_order_book/mod.rs
+++ src/limit_order_book/mod.rs
@@ -1,3 +1,6 @@
+use crate::ops::{update_strategies::ReplaceOrRemove, Update};
+use crate::PriceAndQuantity;
+
 use super::{Asks, Bids};
 #[cfg(feature = "serde")]
 use serde::Deserialize;
@@ -21,6 +24,14 @@ impl LimitOrderBook {
             asks: Asks::new(),
         }
     }
+
+    pub fn add_bid(&mut self, bid: PriceAndQuantity<f64, f64>) {
+        Update::<ReplaceOrRemove>::insert(&mut self.bids, bid)
+    }
+
+    pub fn add_ask(&mut self, ask: PriceAndQuantity<f64, f64>) {
+        Update::<ReplaceOrRemove>::insert(&mut self.asks, ask)
+    }
 }
 
 #[cfg_attr(feature = "serde", derive(Deserialize))]
@@ -33,7 +44,7 @@ pub struct DepthUpdate {
     #[serde(alias = "u")]
     last_update_id: usize,
     #[serde(alias = "b")]
-    bids: Bids,
+    pub bids: Bids,
     #[serde(alias = "a")]
-    asks: Asks,
+    pub asks: Asks,
 }
