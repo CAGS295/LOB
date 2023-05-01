d76775b8b4d18046d247572a5d6df7d4e9afcf30
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-05-01 12:49:41 -0600

    Extend bids and asks

--- src/limit_order_book/mod.rs
+++ src/limit_order_book/mod.rs
@@ -32,6 +32,20 @@ impl LimitOrderBook {
     pub fn add_ask(&mut self, ask: PriceAndQuantity<f64, f64>) {
         Update::<ReplaceOrRemove>::insert(&mut self.asks, ask)
     }
+
+    // Careful, This is a cheap extend and wont respect Ordering.
+    // Use it only if you can guarantee that the concatenation yields an ordered Self.
+    // e.g. You concatenate partitions.
+    pub fn extend(&mut self, other: &Self) {
+        let Self {
+            update_id,
+            bids,
+            asks,
+        } = other;
+        self.bids.extend(bids.iter().copied());
+        self.asks.extend(asks.iter().copied());
+        self.update_id = *update_id;
+    }
 }
 
 #[cfg_attr(feature = "serde", derive(Deserialize))]
